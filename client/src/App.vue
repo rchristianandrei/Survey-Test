b52d1f5a4bb2cec1d8b3e16dd76b06248d0113e2
Andrei Reyes <rchristianandrei@gmail.com>
2025-05-13 14:25:20 +0800

    connect to the back-end

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
+import { ref } from "vue"
 import Question from "./components/Question.vue"
 
 const questions = ["I worry about things.", "I make friends easily.", "I have a vivid imagination." ]
+const answers = ref<string[]>(new Array(questions.length));
 
 function onSubmit(){
-  console.log("submit")
+  const payload = {
+    Q1: answers.value[0],
+    Q2: answers.value[1],
+    Q3: answers.value[2],
+  }
+
+  fetch(import.meta.env.VITE_API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  }).then(() => {
+    alert("Successfully submitted the survey results")
+  }).catch(err => {
+    alert("Failed to submit survey results:")
+    console.log("Failed to submit survey results:", err)
+  });
 }
 </script>
 
 <template>
   <h1 class="text-3xl text-center my-4">Programming Test 1</h1>
   <form @submit.prevent="onSubmit" class="max-w-[1000px] mx-[auto]">
-    <Question v-for="(item, index) in questions" :key="index" :question=item :index=index></Question>
+    <Question v-for="(item, index) in questions" :key="index" :question=item :index=index @change-value="(e) => {answers[index] = e}"></Question>
     <button type="submit" class="rounded cursor-pointer bg-[gray] text-white font-medium p-2 hover:opacity-[.5]">Submit</button>
   </form>
 </template>
--- client/src/components/Question.vue
+++ client/src/components/Question.vue
@@ -6,12 +6,16 @@ const props = defineProps<{
     index: number,
 }>()
 
+const emit = defineEmits<{
+  (e: "changeValue", value:string): void;
+}>()
+
 const choices = [
-  "Very Inaccurate", 
-  "Moderately Inaccurate", 
-  "Neither Accurate Nor Inaccurate", 
-  "Moderately Accurate", 
-  "Very Accurate"
+  ["Very Inaccurate", "1"], 
+  ["Moderately Inaccurate", "2"], 
+  ["Neither Accurate Nor Inaccurate", "3"], 
+  ["Moderately Accurate", "4"], 
+  ["Very Accurate", "5"]
 ]
 const selected = ref<string | null>()
 </script>
@@ -20,7 +24,7 @@ const selected = ref<string | null>()
   <div class="border border-t-[4px] border-t-blue-900 rounded p-3 my-5 shadow-lg">
     <div class="font-semibold">{{ props.index + 1 }}. {{ props.question }}</div>
     <div class="flex justify-around my-2">
-      <div v-for="choice in choices" type="button" :class="`border-2 border-[gray] rounded-xl p-2 cursor-pointer text-blue-900 hover:opacity-[.5] ${choice === selected ? 'bg-[royalblue] text-white': 'bg-[whitesmoke]'}`" @click="selected = choice">
+      <div v-for="([choice, value]) in choices" type="button" :class="`border-2 border-[gray] rounded-xl p-2 cursor-pointer text-blue-900 hover:opacity-[.5] ${choice === selected ? 'bg-[royalblue] text-white': 'bg-[whitesmoke]'}`" @click="selected = choice; emit('changeValue', value);">
         {{ choice }}
       </div>
     </div>
