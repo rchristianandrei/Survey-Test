ced960b592c979a4cb47a562587ec9b0c91be082
Andrei Reyes <rchristianandrei@gmail.com>
2025-05-13 12:48:43 +0800

    design front-end

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import Question from "./components/Question.vue"
+
+const questions = ["I worry about things.", "I make friends easily.", "I have a vivid imagination." ]
+
+function onSubmit(){
+  console.log("submit")
+}
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline text-center">Hello Vue ts</h1>
+  <h1 class="text-3xl text-center my-4">Programming Test 1</h1>
+  <form @submit.prevent="onSubmit" class="max-w-[1000px] mx-[auto]">
+    <Question v-for="(item, index) in questions" :key="index" :question=item :index=index></Question>
+    <button type="submit" class="rounded cursor-pointer bg-[gray] text-white font-medium p-2 hover:opacity-[.5]">Submit</button>
+  </form>
 </template>
-
-<style scoped>
-</style>
---
+++ client/src/components/Question.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+    question: string,
+    index: number,
+}>()
+
+const choices = [
+  "Very Inaccurate", 
+  "Moderately Inaccurate", 
+  "Neither Accurate Nor Inaccurate", 
+  "Moderately Accurate", 
+  "Very Accurate"
+]
+const selected = ref<string | null>()
+</script>
+
+<template>
+  <div class="border border-t-[4px] border-t-blue-900 rounded p-3 my-5 shadow-lg">
+    <div class="font-semibold">{{ props.index + 1 }}. {{ props.question }}</div>
+    <div class="flex justify-around my-2">
+      <div v-for="choice in choices" type="button" :class="`border-2 border-[gray] rounded-xl p-2 cursor-pointer text-blue-900 hover:opacity-[.5] ${choice === selected ? 'bg-[royalblue] text-white': 'bg-[whitesmoke]'}`" @click="selected = choice">
+        {{ choice }}
+      </div>
+    </div>
+  </div>
+</template>
